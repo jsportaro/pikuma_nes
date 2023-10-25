c54b7328183c88b6a7ce381736d1696bf329ed38
Joseph Portaro <jsportaro@outlook.com>
2023-10-25 09:00:45 -0400

    Pointers introduced

--- LoadBackground/loadbackground.asm
+++ LoadBackground/loadbackground.asm
@@ -1,6 +1,13 @@
 .include "consts.inc"
 .include "header.inc"
 .include "reset.inc"
+.include "utils.inc"
+
+.segment "ZEROPAGE"
+Frame:   .res 1              ; Reserve 1 byte to store the number of frames
+Clock60: .res 1              ; Reserve 1 bytes to store a counter that increments every second
+BgPtr:   .res 2              ; Reserve 2 bytes to store a pointer to the background address
+                             ; Lo-byte first followed by hi-byte
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; PRG-ROM code located at $8000
@@ -12,13 +19,12 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 .proc LoadPalette
     ldy #0
-LoopPalette:
-    lda PaletteData, y       ; Loopup byte in ROM
+:   lda PaletteData, y       ; Loopup byte in ROM
     sta PPU_DATA             ; Set value to send to PPU_DATA
                              ; The PPU will automatically increment PPU_ADDR
     iny                      ; Increment y
     cpy #32                  ; Is y 32?
-    bne LoopPalette          ; Not yet, loop
+    bne :-                   ; Not yet, loop
     rts
 .endproc
 
@@ -27,13 +33,26 @@ LoopPalette:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 .proc LoadBackground
     ldy #0
-LoopBackground:   
-    lda BackgroundData, y
+:   lda (BgPtr), y
     sta PPU_DATA
 
     iny
     cpy #255
-    bne LoopBackground
+    bne :- 
+    rts
+.endproc
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Subroutine to load attributes after nametable 0
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.proc LoadAttributes
+    ldy #0
+:   lda AttributeData, y
+    sta PPU_DATA
+
+    iny
+    cpy #16
+    bne :-
     rts
 .endproc
 
@@ -42,25 +61,31 @@ LoopBackground:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 Reset:
     INIT_NES
+
+    lda #0
+    sta Frame
+    sta Clock60
+
 Main:
-    bit PPU_STATUS           ; Every read of PPU_STATUS resets the PPU_ADDR 
-                             ; latch
-    ldx #$3F
-    stx PPU_ADDR             ; Set hi-byte of PPU_ADDR to $3F
-    ldx #$00
-    stx PPU_ADDR             ; Set lo-byte of PPU_ADDR to $00
+    PPU_SETADDR $3F00
     jsr LoadPalette          ; Jump to subroutine LoadPalette
 
-    ;;  Set PPU_ADDR to $2000 and load tiles of the background...
-    bit PPU_STATUS
-    ldx #$20
-    stx PPU_ADDR
-    ldx #0
-    stx PPU_ADDR
+    lda #<BackgroundData     ; Fetch the lo-byte of BackgroundData address
+    sta BgPtr
+    lda #>BackgroundData     ; Fetch the hi-byte of BackgroundData address
+    sta BgPtr + 1
+
+    PPU_SETADDR $2000
     jsr LoadBackground
+
+    PPU_SETADDR $23C0
+    jsr LoadAttributes
 
     lda #%10010000           ; Enable NMI and Set backround to use 2nd pattern table
     sta PPU_CTRL
+    lda #0
+    sta PPU_SCROLL           ; Disable scroll x
+    sta PPU_SCROLL           ; Disable scroll y
     lda #%00011110
     sta PPU_MASK             ; Set PPU_MASK bits to show background
 
@@ -71,6 +96,15 @@ LoopForever:
 ;; NMI interrupt handler
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 NMI:
+    inc Frame
+
+    lda Frame
+    cmp #60
+    bne :+
+    inc Clock60
+    lda #0
+    sta Frame
+:
     rti                      ; Return from interrupt
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -83,8 +117,8 @@ IRQ:
 ;; Hardcoded list of color values in ROM to be loaded by the PPU
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 PaletteData:
-.byte $0F,$2A,$0C,$3A, $0F,$2A,$0C,$3A, $0F,$2A,$0C,$3A, $0F,$2A,$0C,$3A ; Background
-.byte $0F,$10,$00,$26, $0F,$10,$00,$26, $0F,$10,$00,$26, $0F,$10,$00,$26 ; Sprites
+.byte $22,$29,$1A,$0F, $22,$36,$17,$0F, $22,$30,$21,$0F, $22,$27,$17,$0F ; Background palette
+.byte $22,$16,$27,$18, $22,$1A,$30,$27, $22,$16,$30,$27, $22,$0F,$36,$17 ; Sprite palette
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Background data that must be copied to the nametable
@@ -93,11 +127,18 @@ BackgroundData:
 .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$36,$37,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
 .byte $24,$24,$24,$24,$24,$24,$24,$24,$35,$25,$25,$38,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$60,$61,$62,$63,$24,$24,$24,$24
 .byte $24,$36,$37,$24,$24,$24,$24,$24,$39,$3a,$3b,$3c,$24,$24,$24,$24,$53,$54,$24,$24,$24,$24,$24,$24,$64,$65,$66,$67,$24,$24,$24,$24
-.byte $35,$25,$25,$38,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$55,$56,$24,$24,$24,$24,$24,$24,$68,$69,$26,$6a,$24,$24,$24,$24
+.byte $35,$25,$25,$38,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$55,$56,$24,$24,$24,$24,$24,$24,$68,$69,$26,$6a,$2a,$24,$24,$24
 .byte $45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45
 .byte $47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47
 .byte $47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47
 .byte $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Attribute data that is used for 4x4 sections of the background
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+AttributeData:
+.byte %00000000, %00000000, %10101010, %00000000, %11110000, %00000000, %00000000, %00000000
+.byte %11111111, %11111111, %11111111, %11111111, %11111111, %11111111, %11111111, %11111111
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Here we add CHR-ROM data, included from the external .CHR file
